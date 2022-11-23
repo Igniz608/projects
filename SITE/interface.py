de2396512488fcb8060718523c25e96aa22a9eba
Igniz608 <94108774+Igniz608@users.noreply.github.com>
2022-11-23 16:53:20 -0300

    Add files via upload

--- SITE/interface.py
+++ SITE/interface.py
@@ -12,7 +12,6 @@ def dsenha():
      senha["show"]="*"
 
 """
-
 def cadastrar():
      mail = temail.get()
      usuario = nome.get()
@@ -22,38 +21,52 @@ def cadastrar():
      uemail.append(mail)
      print(nomes, senhas, uemail)
 
-
 interface=Tk()
 interface.title("cadastro")
-interface.maxsize(600,450)
-interface.minsize(100,75)
+interface.geometry("600x450")
+# interface.maxsize(600,450)
+# interface.minsize(100,75)
+interface.resizable(False,False)
 interface.config(bg="#6959CD")
 
+img=PhotoImage(file="imagennnn/logopython.png")
+img2=PhotoImage(file="imagennnn/miles.png")
 
+logo=Label(interface)
+logo.pack(fill="x", expand=TRUE, anchor=N, ipady= 30)
+fundo=Label(logo, image=img)
 
+fundo.place(x=0, y=0)
 
-tnome=Label(interface,bg="#6959CD",text="USUÁRIO")
-tnome.grid(row=1,column=1)
+foto=Label(interface, image=img2)
+foto.place(x=-2, y=0)
+
+retangulo=LabelFrame(interface, bg= "#290c3e")
+retangulo.pack(fill="y", expand=TRUE, anchor=CENTER, ipadx= 100, ipady=100, pady=60)
+
+tnome=Label(interface,text="USUÁRIO", bg="#290c3e", foreground='white')
+tnome.place(relx=0.40, rely=0.38)
 nome=Entry(interface)
-nome.grid(row=1,column=2)
+nome.place(relx=0.40, rely=0.44)
 
-email=Label(interface,bg="#6959CD",text="E-MAIL")
-email.grid(row=3,column=1)
-temail=Entry(interface,)
-temail.grid(row=3,column=2)
+email=Label(interface,text="E-MAIL", bg="#290c3e", foreground='white')
+email.place(relx=0.40, rely=0.53)
+temail=Entry(interface,relief= FLAT)
+temail.place(relx=0.40, rely=0.58)
 
-tsenha=Label(interface,bg="#6959CD",text="SENHA")
-tsenha.grid(row=4,column=1)
+tsenha=Label(interface,text="SENHA", bg="#290c3e", foreground='white')
+tsenha.place(relx=0.40, rely=0.68)
 senha=Entry(interface,show="*")
-senha.grid(row=4,column=2)
+senha.place(relx=0.40, rely=0.73)
 
-btlogar=Button(interface,text="LOGAR",width=9, command= entrar)
-btlogar.grid(row=6,column=2)
+botao=Button(interface,text="CADASTRAR",width=9, command= cadastrar, bg="#ff8c32")
+botao.place(relx=0.38, rely=0.79)
 
-botao=Button(interface,text="CADASTRAR",width=9, command= cadastrar)
-botao.grid(row=5,column=2)
+btlogar=Button(interface,text="LOGAR",width=9, command= entrar, bg="#ff8c32")
+btlogar.place(relx=0.51, rely=0.79)
+
 
 psenha=Label(interface,bg="#6959CD",width=10)
-psenha.grid(row=6,column=1)
+psenha.place()
 
 interface.mainloop()
--- SITE/segunda_interface.py
+++ SITE/segunda_interface.py
@@ -6,9 +6,11 @@ from tkinter import messagebox
 from turtle import left, position, width
 
 from setuptools import Command
+
 nomes = []
 senhas = []
 uemail = []
+
 
 def entrar():
 
@@ -23,7 +25,7 @@ def entrar():
                        texto["text"]=("acesso permitido")
                        
                        new=2
-                       url="file:///C:/Users/zollner/Desktop/SITE/SITE/site.html"
+                       url="C:\Users\joao.matos\Desktop\SITE\site.html"
                        webbrowser.open(url,new=new)
                        messagebox.showinfo(title="confirmado",message='logado')
                        interface.destroy()
@@ -33,24 +35,42 @@ def entrar():
             
         else:   
             texto["text"]=("usuario não encontrado")
+    
     interface=Tk()
-
-
     interface.config(bg="#06113c")
-    interface.maxsize(300,330)
-    interface.minsize(300,330)
+    interface.geometry("600x450")
+    # interface.maxsize(300,330)
+    # interface.minsize(300,330)
+    interface.resizable(False,False)
+    interface.config(bg="#6959CD")
+
+   
+    # img3=PhotoImage(file="imagem2/miles2.png")
+
+    
+
+    # foto3=Label(interface, image=img3)
+    # foto3.place(x=-2, y=0)
+
+    # foto3.place(x=0, y=0)
+
+    retangulo2=LabelFrame(interface, bg= "#290c3e")
+    retangulo2.pack(fill="y", expand=TRUE, anchor=CENTER, ipadx= 100, ipady=100, pady=130)
 
 
-    usuario=Entry(interface,width=46,)
-    usuario.place(rely=0.5,height=50,y=-22,x=10)
+    lusuario=Label(interface,text="USUÁRIO", bg="#290c3e", foreground='white')
+    lusuario.place(relx=0.40, rely=0.36)
+    usuario=Entry(interface)
+    usuario.place(relx=0.40, rely=0.42)
 
-
-    senha=Entry(interface,show="*",width=46)
-    senha.place(rely=0.8,height=50,y=-33,x=10)
+    lsenha= Label(interface,text="SENHA", bg="#290c3e", foreground='white')
+    lsenha.place(relx=0.40, rely=0.51)
+    senha=Entry(interface,show="*")
+    senha.place(relx=0.40, rely=0.56)
 
     botao4=Button(interface, text="CONCLUIR",command=logar,width=10,bg="#ff8c32")
-    botao4.place(rely=0.9,x=12,y=-4,height=30)
-    texto=Label(interface,text="",bg="#1c264e",fg="white")
-    texto.place(rely=0.8,relx=0.5,y=30)
+    botao4.place(relx=0.44, rely=0.64)
+    texto=Label(interface,text="",bg="#6959CD",fg="white")
+    texto.place(rely=0.8,relx=0.5,y=28)
 
     interface.mainloop()
